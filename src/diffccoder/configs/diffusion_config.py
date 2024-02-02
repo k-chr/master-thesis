2e64d2d59f0332ac31b7ca941e1b0443cb194aff
Kamil Chrustowski <kachr@duck.com>
2024-02-02 02:09:28 +0100

    Improved DM configuration

--- src/diffccoder/configs/diffusion_config.py
+++ src/diffccoder/configs/diffusion_config.py
@@ -1,23 +1,26 @@
 from dataclasses import dataclass
 
 from diffccoder.configs.base import BaseConfig
-from diffccoder.configs.enums import BetaSchedule, DiffusionModelType
+from diffccoder.configs.enums import BetaSchedule, DiffusionModelType, InferenceSamplerType
 
 
 @dataclass
 class DiffusionConfig(BaseConfig):
-    schedule_sampler: BetaSchedule
-    objective: DiffusionModelType
+    beta_sampler: BetaSchedule = BetaSchedule.COSINE
+    objective: DiffusionModelType = DiffusionModelType.NOISE
     rescale_timesteps: bool = False
-    skip_sample: bool = False
-    infer_self_condition: bool = False
+    self_condition: bool = False
     timesteps: int = 3000
     gen_timesteps: int = 3000
     skip_timesteps: int = 1000
-    ddim_sample: bool = False
     ddim_eta: float = 0
     tgt_len: int = 1024
-    offset_noise_strength: float = 0.0  # offset noise strength - in blogpost, they claimed 0.1 was ideal
+    time_att: bool = True
+    time_dropout: float = 0.2
+    time_channels: int = 512
+    offset_noise_strength: float = 0.1  # offset noise strength - in blogpost, they claimed 0.1 was ideal
     min_snr_loss_weight: bool = False # snr - signal noise ratio
     min_snr_gamma: float = 5 # https://arxiv.org/abs/2303.09556
     end_point_scale: float = 2.0
+    scale_embedding: bool = False
+    inference_sampler: InferenceSamplerType = InferenceSamplerType.DEFAULT
